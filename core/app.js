832133bdc0b4d4060a114ff78f826020484f80ea
alexrobert75 <117197013+alexrobert75@users.noreply.github.com>
2022-12-06 17:12:53 +0100

    Add files via upload

--- core/app.js
+++ core/app.js
@@ -1,7 +1,25 @@
 let link = document.getElementById('link')
 let burger = document.getElementById('burger')
 let ul = document.querySelector('ul')
+let overL = document.querySelector('bikinfo');
+
 
 link.addEventListener('click', function() {
   ul.classList.toggle('open')
 })
+
+const popB = document.getElementById('bikdesc');
+const bikiniB = document.getElementById('bikini');
+
+const revealP = elem => {
+  elem.style.display = "block";
+}
+const hideP = elem => {
+  if (elem.style.display != "none") {
+  elem.style.display = "none";
+  }
+}
+
+bikiniB.onclick = function(){revealP(popB)};
+popB.onclick = function(){hideP(popB)};
+overL.onclick = function(){hideP(popB)};
