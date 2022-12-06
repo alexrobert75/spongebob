6d3a40a2b99e0f048595611073c8c868520c7849
alexrobert75 <117197013+alexrobert75@users.noreply.github.com>
2022-12-06 11:04:06 +0100

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-let link = document.getElementById('link')
-let burger = document.getElementById('burger')
-let ul = document.querySelector('ul')
-
-link.addEventListener('click', function(e) {
-  e.preventDefault()
-  burger.classList.toggle('open')
-  ul.classList.toggle('open')
-})
